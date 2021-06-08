315b77fea0bb94428804e2a4b5de08ed84efe667
Roger Yang <roger.h.yang@gmail.com>
2021-06-08 15:03:15 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.delete_all
 
-User.create(
-  email: 'demo@user.com',
-  password: 123456,
-  first_name: 'Demo',
-  last_name: 'User',
-  birth_date: Faker::Date.birthday(min_age: 18, max_age: 65),
-  gender: Faker::Gender.binary_type,
-  work: Faker::Company.name,
-  school: Faker::University.name,
-  biography: Faker::Job.title
-)
+filenames = [
+  ['owl-6310386_166.jpg', 'forest-1072828_1280.jpg'],
+  ['fox-715588_166.jpg','wintry-2993370_1280.jpg'],
+  ['squirrel-2148641_166.jpg','fall-3089995_1280.jpg'],
+  ['racoon-5400581_166.jpg','forest-602880_1280.jpg'],
+  ['red-panda-1852830_166.jpg','forest-931706_1280.jpg']
+]
+
+User.delete_all
 
 users = []
 
-10.times do
+(0...filenames.size).map do |i|
   users << User.create(
-    email: Faker::Internet.unique.email,
+    email: i == 0 ? 'demo@user.com' : Faker::Internet.unique.email,
     password: 123456,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -34,4 +31,24 @@ users = []
     school: Faker::University.name,
     biography: Faker::Job.title
   )
+
+  if Rails.env.production?
+    basename = File.basename(ActionController::Base.helpers.asset_path(filenames[i][0]))
+    file = Rails.root.join("public", "assets", "seeds", basename)
+  else
+    file = Rails.root.join("app", "assets", "images", "seeds", filenames[i][0])
+  end
+  
+  users[i].profile_picture.attach(io: File.open(file), filename: filenames[i][0])
+
+  if Rails.env.production?
+    basename = File.basename(ActionController::Base.helpers.asset_path(filenames[i][1]))
+    file = Rails.root.join("public", "assets", "seeds", basename)
+  else
+    file = Rails.root.join("app", "assets", "images", "seeds", filenames[i][1])
+  end
+  
+  users[i].cover_photo.attach(io: File.open(file), filename: filenames[i][1])
 end
+
+
