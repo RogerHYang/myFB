aa3777938099c64e76d9a70235d07c37d22790fe
Roger Yang <roger.h.yang@gmail.com>
2021-07-06 19:48:36 -0700

    login demo user

--- frontend/components/session/login_page.jsx
+++ frontend/components/session/login_page.jsx
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 import { useParams, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { login, clearErrors } from "../../actions/session_actions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faAngellist,
+} from "@fortawesome/free-brands-svg-icons";
 
 import Button from "./button";
 
@@ -31,7 +38,9 @@ const LogoAndTagline = styled.div`
 `;
 
 const Logo = styled.div`
-  padding: 112px 0 16px;
+  margin: 112px 0 16px;
+  width: 301px;
+  height: 106px;
 `;
 
 const Tagline = styled.div`
@@ -39,6 +48,10 @@ const Tagline = styled.div`
   line-height: 32px;
   font-size: 1.75rem;
   width: 500px;
+`;
+
+const RightHandSide = styled.div`
+  height: 496px;
 `;
 
 const LoginForm = styled.form`
@@ -76,6 +89,10 @@ const Input = styled.input`
       opacity: 0.5;
     }
   }
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
 `;
 
 const LoginButton = styled(Button)`
@@ -123,33 +140,106 @@ const SignupButton = styled(Button)`
   margin: 6px 0 0;
 `;
 
+const AboutLinks = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 28px 0 0;
+  padding: 0 120px;
+  opacity: 0.25;
+  font-size: 1.5rem;
+  a {
+    color: black;
+  }
+  a:visited {
+    color: black;
+  }
+`;
+
+const Footer = styled.div`
+  margin: 20px 0;
+  width: 980px;
+  color: #737373;
+`;
+
+const Copyright = styled.div`
+  margin: 20px 0;
+  font-size: 0.6875rem;
+`;
+
 export default ({ className }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sessionErrors = useSelector(({ errors }) => errors.session);
+
+  const dispatch = useDispatch();
+
   const handleLogin = (e) => {
     e.preventDefault();
+    dispatch(login({ email, password }));
   };
+
   const loginDemoUser = (e) => {
     e.preventDefault();
+    dispatch(login({ email: "demo@user.com", password: "123456" }));
   };
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    dispatch(clearErrors());
+  };
+
   return (
     <Container>
       <TopSection>
         <TopSectionContent>
           <LogoAndTagline>
-            <Logo>myFB</Logo>
+            <Logo className="logo"></Logo>
             <Tagline>
               Connect with buddies and the forest around you on myFB.
             </Tagline>
           </LogoAndTagline>
-          <LoginForm>
-            <Input placeholder="Email or Phone Number"></Input>
-            <Input placeholder="Password"></Input>
-            <LoginButton onClick={handleLogin}>Log In</LoginButton>
-            <DemoLogin onClick={loginDemoUser}>Log In as Demo User</DemoLogin>
-            <Separator></Separator>
-            <SignupButton>Create New Account</SignupButton>
-          </LoginForm>
+          <RightHandSide>
+            <LoginForm>
+              <Input
+                placeholder="Email or Phone Number"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              ></Input>
+              <Input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              ></Input>
+              <ErrorMessage>
+                {sessionErrors && sessionErrors.map((err) => <p>{err}</p>)}
+              </ErrorMessage>
+              <LoginButton onClick={handleLogin}>Log In</LoginButton>
+              <DemoLogin onClick={loginDemoUser}>Log In as Demo User</DemoLogin>
+              <Separator></Separator>
+              <SignupButton onClick={handleSignup}>
+                Create New Account
+              </SignupButton>
+            </LoginForm>
+            <AboutLinks>
+              <a href="https://www.linkedin.com/in/roger-y-a35595114/">
+                <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+              </a>
+              <a href="https://github.com/RogerHYang">
+                <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+              </a>
+              <a href="https://angel.co/u/roger-yang-11">
+                <FontAwesomeIcon icon={faAngellist}></FontAwesomeIcon>
+              </a>
+            </AboutLinks>
+          </RightHandSide>
         </TopSectionContent>
       </TopSection>
+      <Footer>
+        <Copyright>my Forest Buddies &#169; 2021</Copyright>
+      </Footer>
     </Container>
   );
 };
